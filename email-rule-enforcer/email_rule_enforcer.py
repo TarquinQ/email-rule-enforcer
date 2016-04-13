6c37c66329f5b669655a1885a835349697c51e97
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2016-04-13 23:46:48 +1000

    Python: started to use the new logger module in the XML config parsing.

--- email-rule-enforcer/email_rule_enforcer.py
+++ email-rule-enforcer/email_rule_enforcer.py
@@ -1,15 +1,14 @@
 import modules.python_require_min_pyversion  # checks for py >= 3.4, which we need for newer IMAP TLS support
 import modules.parse_args as parse_args
 import modules.get_xml_configs as get_xml_configs
-import modules.logging
+from modules.logging import log_messages as log_msg
 
 
 prog_version = '0.01-inital-build'
 
-
 parsed_args = parse_args.parse_args(prog_version=prog_version)
 config_files = get_xml_configs.config_files_xml(parsed_args.config_filepath_list[:])
-#config_files.debug_print_config_file_details()
 
 xml_config_tree = config_files.full_config_tree
 
+log_msg.log(30, "Test Warning from logger")
