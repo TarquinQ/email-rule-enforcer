e4b243a2d0a5399b93dbf6dbc23baabc58d1f7e5
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2016-04-13 23:46:00 +1000

    Python: Added print-debugging to logging module, to debug the logger (oh the irony)

--- email-rule-enforcer/modules/logging.py
+++ email-rule-enforcer/modules/logging.py
@@ -16,6 +16,7 @@ class Singleton(type):
 
 
 class log_controller():
+    debug_this_class = False
     """Wraps around the standard Logger class, and builds logs the way we want to."""
     def __init__(self, name, log_level=20, filepath=None):
         # Sets up a new logger, and sets it to Null by default
@@ -23,13 +24,14 @@ class log_controller():
         self.logger = logging.getLogger(name)
         self.formatter_default = self.get_formatter_plainmsg()
         self.handler_null = logging.NullHandler()
-        self.handler_null.setFormatter(self.formatter_default)
         self.logger.addHandler(self.handler_null)
         self.set_loglevel(log_level)
-        if filepath:
-            self.filepath = filepath
-        else:
-            self.set_logger_console()
+        # if filepath:
+        #     self.filepath = filepath
+        # else:
+        #     self.set_logger_console()
+        if self.debug_this_class:
+            print("New log controller intiated; name: ", self.name, " level: ", log_level)
 
     def get_formatter_default(self):
         return self.get_formatter_plainmsg()
@@ -52,10 +54,14 @@ class log_controller():
         self.formatter = self.get_formatter_plainmsg()
         self.handler_console.setFormatter(self.formatter)
         self.logger.addHandler(self.handler_console)
+        if self.debug_this_class:
+            print("Log controller set to console; Controller name: ", self.name)
 
     def set_loglevel(self, log_level=None):
         if log_level: self.log_level = log_level
         self.logger.setLevel(self.log_level)
+        if self.debug_this_class:
+            print("Log controller level changed; Controller name: ", self.name, " New level is ", self.log_level)
 
     def add_logfile(self, filepath, append=False, formatter=None, die_if_file_fails=False):
         """Enables output from this log_controller to a filename"""
@@ -77,12 +83,17 @@ class log_controller():
                 die_with_errormsg('FATAL: Died when opening log file: ', filepath)
             else:
                 self.log('ERROR: Failed to open log file: ', filepath, '\nContinuing with program anyway.')
+        if self.debug_this_class:
+            print("Log controller filename added; Controller name: ", self.name, " new level is ", self.log_level)
 
     def _get_new_filehandler(self, filepath, append=False, die_if_file_fails=False):
         if append:
             mode = 'a'
         else:
             mode = 'w'
+
+        if self.debug_this_class:
+            print("Log controller filehandle addition attempt; Controller name: ", self.name, ", mode: ", mode, ", path: ", filepath)
 
         try:
             new_handler = logging.FileHandler(filepath, mode, encoding='utf8')
@@ -99,6 +110,7 @@ class log_controller():
 
 class log_messages(metaclass=Singleton):
     """Provides unified console and file logging for whole app"""
+    debug_this_class = False
     name_console = 'console'
     name_logfile = 'logfile'
     name_debugfile = 'debugfile'
@@ -107,11 +119,15 @@ class log_messages(metaclass=Singleton):
     controller_logfile = log_controller(name_logfile)
     controller_debugfile = log_controller(name_debugfile)
     log_controllers = {name_console: controller_console, name_logfile: controller_logfile, name_debugfile: controller_debugfile}
+    if debug_this_class:
+        print('New log controllers established: ', log_controllers)
 
     # General Methods to operate on all log_controllers
     @classmethod
     def log(cls, lvl, msg, *args, **kwargs):
         for log_controller in cls.log_controllers.values():
+            if cls.debug_this_class:
+                print('Now logging a message for controller. Name: ', log_controller.name, ", msg: ", msg, str(*args), str(**kwargs))
             log_controller.logger.log(lvl, msg, *args, **kwargs)
 
     @classmethod
