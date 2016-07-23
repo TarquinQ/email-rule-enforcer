ba6d292d4dc8af1505ec0a4f1e34fecd3338b0a4
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2016-07-24 00:46:49 +0930

    Python: renamed new emails module

--- email-rule-enforcer/modules/email/make_new_emails.py
+++ email-rule-enforcer/modules/email/make_new_emails.py
@@ -2,28 +2,29 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.nonmultipart import MIMENonMultipart
 from email.mime.text import MIMEText
 from email.headerregistry import Address
+from modules.logging import LogMaster
 
 # type: Address(display_name='', username='', domain='', addr_spec=None)
-# sending: smtplib.SMTP().send_message(msg, from_addr=None, to_addrs=None, mail_options=[], rcpt_options=[])
+# sending: smtplib.SMTP().send_message(msg, email_fromaddr=None, email_toaddrs=None, mail_options=[], rcpt_options=[])
 
 
-def new_email_multipart(from_, to_, subject, bodytext, files_to_attach):
+def new_email_multipart(email_from, email_to, subject, bodytext, files_email_toattach):
     # Create the container (outer) email message.
     msg = MIMEMultipart()
     msg['Subject'] = subject
-    msg['From'] = from_
-    if type(to_, list):
+    msg['From'] = email_from
+    if type(email_to, list):
         COMMASPACE = ', '
-        _to = COMMASPACE.join(to_)
+        _to = COMMASPACE.join(email_to)
     else:
-        _to = to_
+        _to = email_to
     msg['To'] = _to
 
     body = MIMEText(bodytext)
     msg.attach(body)
 
     # Assume we know that the image files are all in Text format
-    for file in files_to_attach:
+    for file in files_email_toattach:
         # Open the files in binary mode.  Let the MIMEText class automatically
         # guess the specific type.
         with open(file, 'rb') as fp:
@@ -33,26 +34,26 @@ def new_email_multipart(from_, to_, subject, bodytext, files_to_attach):
     return msg
 
 
-def new_email_nonmultipart(from_, to_, subject, bodytext, cc=None, bcc=None):
+def new_email_nonmultipart(email_from, email_to, subject, bodytext, cc=None, bcc=None):
     # Create the container (outer) email message.
     #msg = MIMENonMultipart()
     msg = MIMEText(bodytext)
     msg['Subject'] = subject
-    msg['From'] = from_
-    if type(to_, list):
+    msg['From'] = email_from
+    if type(email_to, list):
         COMMASPACE = ', '
-        _to = COMMASPACE.join(to_)
+        _to = COMMASPACE.join(email_to)
     else:
-        _to = to_
+        _to = email_to
     msg['To'] = _to
 
     return msg
 
 
-def new_email(from_='', to_='', subject='', bodytext='', files_to_attach=None, cc=None, bcc=None):
-    if files_to_attach is None:
-        return new_email_nonmultipart(from_, to_, subject, bodytext, cc, bcc)
+def new_email(email_from='', email_to='', subject='', bodytext='', files_email_toattach=None, cc=None, bcc=None):
+    if files_email_toattach is None:
+        return new_email_nonmultipart(email_from, email_to, subject, bodytext, cc, bcc)
     else:
-        return new_email_multipart(from_, to_, subject, bodytext, files_to_attach, cc, bcc)
+        return new_email_multipart(email_from, email_to, subject, bodytext, files_email_toattach, cc, bcc)
 
 
