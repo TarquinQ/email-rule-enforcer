69efb8905fb4930c2889c5142b230e45eddecb7a
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2016-09-12 16:49:38 +0930

    Python: added ability to clone Config without passwords, for logfile output

--- email-rule-enforcer/modules/models/Config.py
+++ email-rule-enforcer/modules/models/Config.py
@@ -1,2 +1,8 @@
 class Config(dict):
-    pass
+    def clone_nopasswd(self):
+        new_copy = self.copy()
+        new_copy['imap_password'] = '**Password Hidden**'
+        new_copy['smtp_password'] = '**Password Hidden**'
+        new_copy['imap_folders_to_exclude'] = '**Folder List Here**'
+        return new_copy
+
