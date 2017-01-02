3ac06855a9b511eca97a3331ea2b345100de166a
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2017-01-02 23:03:22 +1030

    Python: minor changes, no production impact

--- email-rule-enforcer/modules/models/GlobalEvents.py
+++ email-rule-enforcer/modules/models/GlobalEvents.py
@@ -4,6 +4,6 @@ import threading
 class GlobalEvents():
     def __init__(self):
         self.shutdown_evt = threading.Event()
-        self.reread_config_evt = threading.Event()
+        self.full_sync_required = threading.Event()
         self.imap_perm_fail = threading.Event()
 
--- email-rule-enforcer/modules/models/GracefulShutdown.py
+++ email-rule-enforcer/modules/models/GracefulShutdown.py
@@ -1,4 +1,6 @@
-def graceful_shutdown(imap_connection=None):
+def graceful_shutdown(imap_connection=None, db=None):
     if imap_connection is not None:
         imap_connection.disconnect()
+    if db is not None:
+        db.close()
 
