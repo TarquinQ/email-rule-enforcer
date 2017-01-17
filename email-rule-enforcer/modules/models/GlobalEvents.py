32f434a75322008b0167f5ee2590bfd3884a0394
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2017-01-18 00:28:30 +1030

    Python: Removing GlobalEvents (to be replaced)

--- email-rule-enforcer/modules/models/GlobalEvents.py
+++
@@ -1,9 +0,0 @@
-import threading
-
-
-class GlobalEvents():
-    def __init__(self):
-        self.shutdown_evt = threading.Event()
-        self.full_sync_required = threading.Event()
-        self.imap_perm_fail = threading.Event()
-
