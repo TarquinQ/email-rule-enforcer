aae468c785745185af6bef907c33eb8386a5ec35
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2017-01-18 14:42:54 +1030

    Python: added deadline-based TimerFlags

--- email-rule-enforcer/modules/models/TimerFlag.py
+++ email-rule-enforcer/modules/models/TimerFlag.py
@@ -1,17 +1,54 @@
 import threading
 import datetime
+import re
 
 
 class TimerFlag():
-    def __init__(self, name=None, default_time_incr=None):
+    def __init__(self, name=None):
         self.name = name
         self.flag = threading.Event()
         self.timer = threading.Timer(0, print)  # dummy values just to create a Timer
         self.timer.daemon = True
-        self.set_default_time_incr(default_time_incr)
+        self.next_deadline = datetime.datetime.now()
+        self.set_default_time_incr(21600)  # 6 hours
+        self.is_deadline_based = False
+        self.deadline_base = '25:00'
 
     def set_default_time_incr(self, time_incr):
         self.default_time_incr = time_incr
+
+    def set_deadline_base(self, deadline_base):
+        if re.match('[0-9][0-9]:[0-9][0-9]', str(deadline_base)):
+            self.deadline_base = deadline_base
+            self.deadline_base_hours = int(deadline_base.split(':')[0])
+            self.deadline_base_mins = int(deadline_base.split(':')[1])
+            #self.deadline_base_seconds = (self.deadline_base_hours*3600)+(deadline_base_minutes*60)
+            self.is_deadline_based = True
+
+    def reset_next_deadline_from_base(self):
+        """Derives time of next deadline according to the requested base"""
+        now = datetime.datetime.now()
+        compare_hours = now.hour
+        compare_mins = now.minute
+        target_hours, target_mins = self.deadline_base_hours, self.deadline_base_mins
+        # convert to seconds, and make sure that we account for the 00:00 > 23:00 conundrum....
+        if target_hours < compare_hours:
+            target_hours += 24
+        target_s = ((60 * target_hours) + target_mins) * 60
+        compare_s = ((60 * compare_hours) + compare_mins) * 60
+        # Now we just work out when the next run should be, in order to hit the timing_base
+        while compare_s < target_s:
+            target_s = target_s - self.default_time_incr
+        next_deadline_hours = ((target_s//60)//60)
+        next_deadline_mins = int((target_s//60) % 60)
+        if next_deadline_hours > 23:
+            next_deadline_hours = next_deadline_hours - 24
+            next_deadline_days = 1
+        else:
+            next_deadline_days = 0
+        next_deadline = now + datetime.timedelta(days=next_deadline_days)
+        next_deadline.replace(hour=next_deadline_hours, minute=next_deadline_mins)
+        self.reset_timer_datetime(next_deadline)
 
     def reset_timer(self, next):
         """Sets next timer. next is passed in as seconds"""
@@ -22,11 +59,10 @@ class TimerFlag():
         self.timer.start()
 
     def reset_timer_default(self):
-        if self.default_time_incr is not None:
+        if self.is_deadline_based is False:
             self.reset_timer(self.default_time_incr)
         else:
-            raise ValueError('No default time_incr has been set on TimerFlag object, \
-                but default methods have been called requiring this value')
+            self.reset_next_deadline_from_base()
 
     def reset_timer_datetime(self, next_deadline):
         """Sets next timer. next_deadline is passed in as datetime"""
