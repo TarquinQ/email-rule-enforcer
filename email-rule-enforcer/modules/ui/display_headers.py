5615b5ba62af9250be378219017ed2b2600f9ec4
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2017-01-19 17:16:43 +1030

    Python: updated initial Headers to show more info to user

--- email-rule-enforcer/modules/ui/display_headers.py
+++ email-rule-enforcer/modules/ui/display_headers.py
@@ -18,14 +18,25 @@ def get_header_postconfig(config):
 **
 ** Configuration section completed. We will now attempt to:
 **  * Open log files
-**  * Connect to IMAP server \'%s:%s\' (SSL?: %s)
+**  * Open caching database (%s)
+**  * Connect to IMAP server \'%s:%s\' (SSL: %s)
 **             with Username \"%s\"%s
-**
-*********************************************************************
-''' % (config['imap_server_name'],
+**  * Perform email sync & rule actions
+''' % (config['database_filename'],
+        config['imap_server_name'],
         config['imap_server_port'],
         config['imap_use_tls'],
         config['imap_username'],
         '')
+
+    if config['daemon_mode'] is True:
+        ret_str += '**  * Stay alive and keep processing new messages'
+    else:
+        ret_str += '**  * Gracefully exit'
+
+    ret_str += '''
+**
+*********************************************************************
+'''
     return ret_str
 
