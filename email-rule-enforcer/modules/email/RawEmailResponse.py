3afc2e01545db3d955bfbc88b851bdd925904d6d
Tarquin Qualcox <tarquin.qualcox@gmail.com>
2017-03-14 23:02:01 +1030

    Revert "Python: updated UI Display Headers and Footers"
    This reverts commit e61478ed819e1afa7a94722481a78e85bc9f3a44.

---
+++ email-rule-enforcer/modules/email/RawEmailResponse.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+
+
+class RawEmailResponse(namedtuple('RawEmailResponse', ['raw_email_bytes', 'flags', 'size', 'server_date'])):
+    def __new__(self, raw_email_bytes=None, flags=None, size=None, server_date=None):
+        return super().__new__(self, raw_email_bytes, flags, size, server_date)
+
+
+# class RawEmailResponse():
+#     def __init__(self, raw_email_bytes=None, flags=None, size=None, server_date=None):
+#         self.raw_email_bytes = raw_email_bytes
+#         self.size = size
+#         self.flags = flags
+#         self.server_date = server_date
+
+#     def __str__(self):
+#         ret_str = str(self.__class__.__name__)
+#         ret_str += ': Size {0}, Flags: {1}, ServerDate: {2}, raw_email:\n{3}'.format(
+#             self.size,
+#             self.flags,
+#             self.server_date,
+#             self.raw_email_bytes
+#         )
+#         return ret_str
+
+#     def __repr__(self):
+#         return self.__str__()
